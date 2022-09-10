02abfdc6e9ace040843193c7e52c18dbb59ae8cf
tuhinal <alauddintuhin1411@gmail.com>
2022-09-10 18:35:21 +0600

    enum, tuple and union type

--- index.ts
+++ index.ts
@@ -29,9 +29,36 @@ student.name = 'Tasin';
 student.email = 'tasin@gmail.com';
 student.isActive = true;
 student.hobby = ['Playing', 'gossiping', 'outing'];
-
+student.roles = ['Student', 1];
 console.log('the student: ',student);
 
+function combineType(num1: number | string | boolean, num2: number | string | boolean) {
+
+    if (typeof num1 === "boolean" && typeof num2 === "boolean") {
+        console.log('Both are boolean type');
+    }else if (typeof num1 === "string" && typeof num2 === "string") {
+        console.log('Both are String type');
+    }else if (typeof num1 === "number" && typeof num2 === "number") {
+        console.log('Both are number type');
+    } else {
+        console.log('Wrong type');
+    }
+
+}
+
+ export enum RoleEnum {
+    SUPER_ADMIN = "Super Admin",
+    ADMIN = "Admin",
+    ALL_USER = 'All user'
+}
+
+student.role = RoleEnum.SUPER_ADMIN;
+combineType('Tuhin', 'Selim');
+combineType('Tuhin', 25);
+combineType(26, 25);
+combineType(true, true);
+
+console.log('the student role: ',student.role);
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Practice</h1>`;
 
--- student.ts
+++ student.ts
@@ -1,16 +1,22 @@
+import {RoleEnum} from "./index";
+
 export class Student {
     name: string;
     age: number;
     email: string
     hobby: Array<string> = [];
     isActive: boolean;
+    roles: [string , number];
+    role: RoleEnum
 
-
-    constructor(name: string, age: number, email: string, hobby: Array<string>, isActive: boolean) {
+    constructor(name: string, age: number, email: string, hobby: Array<string>, isActive: boolean,
+                roles: [string , number], role) {
         this.name = name;
         this.age = age;
         this.email = email;
         this.hobby = hobby;
         this.isActive = isActive;
+        this.roles= roles;
+        this.role= role;
     }
 }
