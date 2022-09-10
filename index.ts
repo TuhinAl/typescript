253b40d7631e86a3b27bc9cec2a52d821d1ec4ca
tuhinal <alauddintuhin1411@gmail.com>
2022-09-10 10:19:17 +0600

    basic

--- index.ts
+++ index.ts
@@ -8,6 +8,9 @@ console.log('test')
 function add(num1: number, num2: number) : number {
     return num1 + num2;
 }
+function addStringAndNumber(num1: string, num2: number) : number {
+    return +num1 * num2;
+}
 function stringAdd(num1: string, num2: string) : number {
     return +(num1 + num2);
 }
@@ -15,6 +18,7 @@ function stringAdd(num1: string, num2: string) : number {
 console.log(add(10, 12));
 console.log(add(+'22', +'15'));
 console.log(stringAdd('22', '15'));
+console.log(addStringAndNumber('22', 2));
 
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Practice</h1>`;
