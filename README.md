25c409091108486b431439514015c79e0f916eba
altuhin <alauddin.tuhin@egeneration.co>
2022-09-10 10:11:51 +0600

    typescript basic

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # typescript-7fuasd
-
+#to run application: npm run serve
 [Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-7fuasd)
--- index.ts
+++ index.ts
@@ -2,5 +2,19 @@
 import './style.css';
 
 // Write TypeScript code!
+
+console.log('test')
+
+function add(num1: number, num2: number) : number {
+    return num1 + num2;
+}
+function stringAdd(num1: string, num2: string) : number {
+    return +(num1 + num2);
+}
+
+console.log(add(10, 12));
+console.log(add(+'22', +'15'));
+console.log(stringAdd('22', '15'));
+
 const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+appDiv.innerHTML = `<h1>TypeScript Practice</h1>`;
